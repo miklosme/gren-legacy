19fb86d43ae0e4e0a1d68d3142eea3bc997bac99
miklosme <m.miklos05@gmail.com>
2018-07-09 13:59:04 +0200

    wip

--- cli.js
+++ cli.js
@@ -6,7 +6,7 @@ var majorVer = parseInt(ver.split('.')[0], 10);
 var cli;
 
 if (majorVer < 4) {
-    throw new Error('Node version ' + ver + ' is not supported in Glow, please use Node.js 4.0 or higher.');
+    throw new Error('Node version ' + ver + ' is not supported in Gren, please use Node.js 4.0 or higher.');
 } else if (majorVer < 8) {
     cli = require('./dist/legacy/cli').default;
 } else {
