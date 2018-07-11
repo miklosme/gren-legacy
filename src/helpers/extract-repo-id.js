5490fa150d9eab30c53153b52cc4ca3509a20fdd
miklosme <m.miklos05@gmail.com>
2018-07-11 12:40:58 +0200

    handle more type of repository fields

--- src/helpers/extract-repo-id.js
+++ src/helpers/extract-repo-id.js
@@ -1,4 +1,4 @@
 module.exports = function extractRepoId(repositoryPath) {
-    const [owner, name] = repositoryPath.replace(/.*github\.com[:\/]([\w|-]+\/[\w|-]+)\.git/, (_, p) => p).split('/');
+    const [owner, name] = repositoryPath.replace(/.*github\.com[:\/]([\w|-]+\/[\w|-]+)(\.git)?/, (_, p) => p).split('/');
     return { owner, name };
 }
