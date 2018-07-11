d983585443046a4ed749f246e604c1d41b2b6f52
miklosme <m.miklos05@gmail.com>
2018-07-11 17:22:45 +0200

    properly find the pr number

--- src/github/update-bot-comment.js
+++ src/github/update-bot-comment.js
@@ -1,5 +1,6 @@
 const octokit = require('@octokit/rest')();
 const getRepoInfo = require('../helpers/get-repo-info');
+const getBranchName = require('../helpers/get-branch-name');
 const { reportId } = require('../commons/constants');
 
 // graphql api does not support editing comments, so we need to use v3
@@ -11,9 +12,24 @@ octokit.authenticate({
 
 module.exports = async function updateBotComment(_, body) {
     const { owner, name: repo } = getRepoInfo();
+    const branchName = getBranchName();
 
-    // for some reason the editing endpoint doesn't work with the graphql id (it should), so let's find it again
-    const { data } = await octokit.issues.getComments({ owner, repo, number: 1 });
+    // for some reason the editing endpoint doesn't work with the graphql id (it should)
+    // let's find the non-hashed id
+
+    const prNumber = await octokit.pullRequests.getAll({ owner, repo, state: 'open' }).then(result => {
+        try {
+            return result.data.filter(pr => pr.head.ref === branchName)[0].number;
+        } catch (error) {
+            throw new Error('could not find pr on github');
+        }
+    });
+
+    const { data } = await octokit.issues.getComments({
+        owner,
+        repo,
+        number: prNumber,
+    });
     const commentId = data.find(comment => comment.body.includes(reportId)).id;
 
     return octokit.issues.editComment({ owner, repo, comment_id: commentId, body });
