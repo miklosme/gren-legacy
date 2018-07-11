84ac1be4dfd880ba66d2741e468cf3cb8eb14843
miklosme <m.miklos05@gmail.com>
2018-07-11 10:10:53 +0200

    handle repository fiald as object in package.json

--- src/helpers/get-repo-info.js
+++ src/helpers/get-repo-info.js
@@ -1,7 +1,21 @@
 const extractRepoId = require('./extract-repo-id');
 const getPackageJson = require('./get-package-json');
 
+//   "repository": {
+//     "type": "git",
+//     "url": "https://github.com/EclecticIQ/platform-ui"
+//   },
+
 module.exports = function getRepoInfo() {
     const { repository } = getPackageJson();
+    if (!repository) {
+        throw new Error('you must set the `repository` field in package json');
+    }
+    if (typeof repository === "object") {
+        if (!repository.url) {
+            throw new Error('you must set the `repository.url` field in package json');
+        }
+        return extractRepoId(repository.url);
+    }
     return extractRepoId(repository);
 };
